d89d4e040467283a6c2d43886f041a2c4792a242
Raya Berova <rayaberova7@gmail.com>
2024-04-26 09:40:52 +0000

    selectionner les images dans une bounding_box et plotter les polygones sur les images de la zone

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,11 +1,12 @@
 import re
 from typing import List
 from pyproj import Transformer
+from shapely.geometry import box, Point
 
 
 def find_image_of_point(
-    coordinates: List,
-    list_filepaths: str,
+    coordinates: List(float),
+    list_filepaths: List(str),
     crs: int,
     coord_gps: bool = True,
 ) -> str:
@@ -28,8 +29,9 @@ def find_image_of_point(
             The path of the image containing the point.
 
     Examples:
-        >>> point = (-12.768023, 45.190708)
+        >>> point = (-12.783007, 45.221377)
         >>> image_path = find_image_of_point(point, list_images_mayotte_2020, 4471, True)
+        'ORT_2020052526670967_0524_8587_U38S_8Bits'
     """
 
     if coord_gps:
@@ -41,9 +43,9 @@ def find_image_of_point(
     else:
         x, y = coordinates
 
-    # Retrieve left-top coordinates
-    delimiters = ["-", "_"]
+    point = Point(x, y)
 
+    delimiters = ["-", "_"]
     pattern = "|".join(delimiters)
 
     for filename in list_filepaths:
@@ -54,9 +56,61 @@ def find_image_of_point(
         top = float(split_filename[3]) * 1000
         right = left + 1000.0
         bottom = top - 1000.0
+        bbox = box(left, bottom, right, top)
 
-        if left <= x <= right:
-            if bottom <= y <= top:
-                return filename
-    else:
-        return "The point is not find in the folder."
+        # Vérifier si le point est inclus dans la bounding box
+        if bbox.contains(point):
+            return filename
+
+    return "The point is not find in the folder."
+
+
+def find_images_of_bb(
+    bbox: box,
+    list_filepaths: List(str),
+) -> List(str):
+    """
+    Gives the images in the folder which are in the bounding box.
+    This method is based on the filenames of the pleiades images.
+
+    Args:
+        bbox (box):
+            bounding box in the CRS of the isle.
+        folder_path (str):
+            The path of the folder in which we search the images contained in
+            the bounding box.
+
+    Returns:
+        List(str):
+            The list of the images paths included in the bounding box.
+
+    Examples:
+        >>> left, bottom, right, top = (523500.0, 8586500.0, 524500.0, 8587500.0)
+        >>> bounding_box = box(left, bottom, right, top)
+        >>> find_images_of_bb(bounding_box, list_images_mayotte_2020)
+        ['ORT_2020052526670967_0523_8587_U38S_8Bits.jp2',
+        'ORT_2020052526670967_0523_8588_U38S_8Bits.jp2',
+        'ORT_2020052526670967_0524_8587_U38S_8Bits.jp2',
+        'ORT_2020052526670967_0524_8588_U38S_8Bits.jp2']
+    """
+    # Retrieve left-top coordinates
+    delimiters = ["-", "_"]
+
+    pattern = "|".join(delimiters)
+
+    filepaths_images_in_bb = []
+    for filename in list_filepaths:
+        filename = filename.split("/")[-1]
+        split_filename = re.split(pattern, filename)
+
+        left = float(split_filename[2]) * 1000
+        top = float(split_filename[3]) * 1000
+        right = left + 1000.0
+        bottom = top - 1000.0
+
+        bbox2 = box(left, bottom, right, top)
+
+        if bbox.intersects(bbox2):
+            filepaths_images_in_bb.append(filename)
+
+    return filepaths_images_in_bb

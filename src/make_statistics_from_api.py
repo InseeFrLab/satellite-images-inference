08c46b48095775c3555605464f359eb0fe72bcbe
ThomasFaria <thomas.faria@ensae.fr>
2025-05-20 08:38:11 +0000

    include statistics in the workflow

--- src/make_statistics_from_api.py
+++ src/make_statistics_from_api.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+from datetime import datetime
 
 import aiohttp
 import geopandas as gpd
@@ -161,7 +162,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--year",
         type=int,
-        choices=[2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
+        choices=range(2017, datetime.now().year + 2),
         metavar="N",
         default=2020,
         help="Year of the dataset to make predictions on",

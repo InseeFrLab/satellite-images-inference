d676302d0304961fe298148aed4ce4bc3123c09c
meilame-tayebjee <meilame.tayebjee@polytechnique.edu>
2025-07-09 08:53:14 +0000

    fix: avoid bugs when AWS_SESSION_TOKEN is not None 	modified:   app/utils.py

--- app/utils.py
+++ app/utils.py
@@ -39,6 +39,7 @@ def get_file_system() -> S3FileSystem:
         client_kwargs={"endpoint_url": f"https://{os.environ['AWS_S3_ENDPOINT']}"},
         key=os.environ["AWS_ACCESS_KEY_ID"],
         secret=os.environ["AWS_SECRET_ACCESS_KEY"],
+        token=None,
     )
 
 
